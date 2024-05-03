c7a8eddbb488206e8280ff82bbdd5df8d73069b9
ArslanH <ArslanH@DSI-ARSLANH>
2024-05-04 00:31:42 +0500

    Add project files.

---
+++ Desktop_POC_Application/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Desktop_POC_Application
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Desktop_POC_Application/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Desktop_POC_Application
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
